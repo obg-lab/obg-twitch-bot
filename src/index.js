0a412cfad287db4aa5e2df52f113c7c266fa14ab
Bruno Germano <bruno@egermano.com>
2021-05-28 14:45:47 -0300

    first impressions

--- src/index.js
+++ src/index.js
@@ -5,22 +5,45 @@ dotenv.config();
 import * as tmi from 'tmi.js';
 import { logger } from './logger/index.js';
 
-const client = new tmi.Client({
+const options = {
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN,
+  },
   connection: {
     reconnect: true,
   },
   channels: [process.env.CHANNEL_NAME],
-});
+};
 
-client.connect();
+try {
+  const client = new tmi.Client(options);
+  
+  client.on('message', async (channel, context, message, self) => {
+    logger.info(
+      {
+        channel,
+        user: context.username,
+        message,
+      },
+      'Channel and user info'
+    );
 
-client.on('message', async (channel, context, message) => {
-  logger.info(
-    {
-      channel,
-      user: context.username,
-      message,
-    },
-    'Channel and user info'
-  );
-});
+    switch (message) {
+      case `!ola`:
+        client.say(channel, `Hello World! @${context.username}`);
+        break;
+    
+      default:
+        break;
+    }
+  });
+
+  client.on('connected', (addr, port) => {
+    logger.info({ addr, port }, 'Bot connected');
+  });
+
+  client.connect();
+} catch (error) {
+  logger.error({ error }, 'Bot connection error');
+}
