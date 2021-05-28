1ce967be3ff1ba3bf82d513de3e1d1ccc0483733
Bruno Germano <bruno@egermano.com>
2021-05-28 15:47:34 -0300

    refactoring chant connect. say, events

---
+++ src/components/chatClient.js
@@ -0,0 +1,46 @@
+import EventEmitter from 'events';
+import * as tmi from 'tmi.js';
+import * as dotenv from 'dotenv';
+import { logger } from '../logger/index.js';
+
+dotenv.config();
+
+class ChatEmitter extends EventEmitter {}
+
+export const chatEvents = new ChatEmitter();
+
+const options = {
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN,
+  },
+  connection: {
+    reconnect: true,
+  },
+  channels: [process.env.CHANNEL_NAME],
+};
+
+export const client = new tmi.Client(options);
+
+client.on('message', async (channel, context, message, self) => {
+  logger.info({ channel, context, message, self }, 'got a new message');
+  chatEvents.emit('message', channel, context, message, self);
+});
+
+export const connect = async () => {
+  return new Promise((resolve, reject) => {
+    client.on('connected', (addr, port) => {
+      logger.info({ addr, port }, 'Bot connected');
+      resolve();
+    });
+
+    try {
+      client.connect();
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+export const say = async (message) =>
+  client.say(process.env.CHANNEL_NAME, message);
--- src/index.js
+++ src/index.js
@@ -1,49 +1,10 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-import * as tmi from 'tmi.js';
+import * as chatClient from './components/chatClient.js';
 import { logger } from './logger/index.js';
 
-const options = {
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN,
-  },
-  connection: {
-    reconnect: true,
-  },
-  channels: [process.env.CHANNEL_NAME],
-};
-
-try {
-  const client = new tmi.Client(options);
-  
-  client.on('message', async (channel, context, message, self) => {
-    logger.info(
-      {
-        channel,
-        user: context.username,
-        message,
-      },
-      'Channel and user info'
-    );
-
-    switch (message) {
-      case `!ola`:
-        client.say(channel, `Hello World! @${context.username}`);
-        break;
-    
-      default:
-        break;
-    }
-  });
-
-  client.on('connected', (addr, port) => {
-    logger.info({ addr, port }, 'Bot connected');
-  });
-
-  client.connect();
-} catch (error) {
-  logger.error({ error }, 'Bot connection error');
-}
+(async () => {
+  try {
+    await chatClient.connect();
+  } catch (error) {
+    logger.error({ error }, 'Error on bootstrap chat.');
+  }
+})();
