2249cf696e30a4c0eacaa042ee7c02f03da4babf
Bruno Germano <bruno@egermano.com>
2021-05-28 16:47:01 -0300

    use the NODE_ENV var

--- src/logger/index.js
+++ src/logger/index.js
@@ -2,4 +2,4 @@ import pino from 'pino';
 
 const pinoLogger = pino();
 
-export const logger = pinoLogger.child({ environment: 'dev' });
+export const logger = pinoLogger.child({ environment: process.env.NODE_ENV || 'dev' });
