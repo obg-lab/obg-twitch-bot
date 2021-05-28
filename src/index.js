c0a6833f0b2ca8d93a3f95c0c1a54cd0ae17cdc1
Bruno Germano <bruno@egermano.com>
2021-05-28 16:21:32 -0300

    implement skills

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import * as chatClient from './components/chatClient.js';
 import { logger } from './logger/index.js';
+import { scheduled } from './skills/index.js';
 
 (async () => {
   try {
     await chatClient.connect();
+
+    scheduled();
+
   } catch (error) {
-    logger.error({ error }, 'Error on bootstrap chat.');
+    logger.error(error, 'Error on bootstrap chat.');
   }
 })();
