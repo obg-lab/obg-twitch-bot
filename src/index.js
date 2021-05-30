4a52dbf85abb75c5d46bcce0f061e90fa11a812a
Bruno Germano <bruno@egermano.com>
2021-05-29 21:42:57 -0300

    update main file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import * as chatClient from './components/chatClient.js';
 import { logger } from './logger/index.js';
-import { scheduled } from './skills/index.js';
+import { scheduled } from './skills/schedule.js';
 
 (async () => {
   try {
     await chatClient.connect();
 
     scheduled();
-
   } catch (error) {
     logger.error(error, 'Error on bootstrap chat.');
   }
