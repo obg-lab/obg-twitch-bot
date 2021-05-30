c79ef2db3eaed18640ad605a57ac4c7688c15d61
Bruno Germano <bruno@egermano.com>
2021-05-29 21:42:42 -0300

    refactoring schedule skill

---
+++ src/skills/schedule.js
@@ -0,0 +1,41 @@
+import cron from 'node-cron';
+import * as _ from 'lodash';
+import { say } from '../components/chatClient.js';
+import { logger } from '../logger/index.js';
+import { load } from '../service/configFiles.js';
+import { isUserStreaming } from '../service/stream.js';
+
+export const scheduled = () => {
+  try {
+    const file = load('scheduled');
+
+    for (let i = 0; i < file.length; i++) {
+      const schedule = file[i];
+
+      logger.info(
+        { schedule, isValid: cron.validate(schedule.schedule) },
+        'Setup schedule cron job.'
+      );
+
+      cron.schedule(schedule.schedule, async () => {
+        const { messages, name, streaming, username } = schedule;
+
+        try {
+          const index = Math.floor(Math.random() * messages.length);
+          const message = messages[index];
+
+          logger.info({ message }, 'Sending message');
+
+          if (streaming && isUserStreaming(username)) {
+            return say(message);
+          }
+        } catch (error) {
+          logger.error(error, `Error sending scheduled ${name} message`);
+        }
+      });
+    }
+  } catch (error) {
+    logger.error(error, 'Error on scheduled starts');
+    throw error;
+  }
+};
