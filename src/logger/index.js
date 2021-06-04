0a8cfa45ebaa39c9eaa3650d65d0faac7f458703
Bruno Germano <bruno@egermano.com>
2021-06-04 13:17:05 -0300

    improve logger

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,5 +1,12 @@
 import pino from 'pino';
+import * as pkginfo from '../../package.json';
 
 const pinoLogger = pino();
 
-export const logger = pinoLogger.child({ environment: process.env.NODE_ENV || 'dev' });
+export const logger = pinoLogger.child({
+  environment: process.env.NODE_ENV || 'dev',
+  service: {
+    name: pkginfo.name,
+    version: pkginfo.version,
+  },
+});
