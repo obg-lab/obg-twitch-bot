694bab73820c43c9476e3bd0c5eeed086b11c34e
Bruno Germano <bruno@egermano.com>
2021-06-09 20:28:02 -0300

    add Event sub and end process listener

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import * as chatClient from './components/chatClient.js';
+import * as eventSub from './components/eventSub.js';
 import { logger } from './logger/index.js';
 import { scheduled } from './skills/schedule.js';
 import { faq } from './skills/faq.js';
 import { shortcuts } from './skills/shortcut.js';
+
+const terminate = async () => {
+  await eventSub.stop();
+  process.exit();
+}
+
+process.on('SIGINT', terminate);
+process.on('SIGTERM', terminate);
 
 (async () => {
   try {
@@ -11,7 +20,9 @@ import { shortcuts } from './skills/shortcut.js';
     scheduled();
     faq();
     shortcuts();
-    
+   
+    await eventSub.listen();
+
   } catch (error) {
     logger.error(error, 'Error on bootstrap chat.');
   }
